1a9e2c61610c41be907001d09df21df35995e705
Jedeye <jody.septiawan5@gmail.com>
2022-08-03 15:27:00 +0700

    Day 6

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const bcrypt = require('bcrypt');
+const session = require('express-session')
+const flash = require('express-flash')
 
 const db = require('./connection/db')
 
@@ -6,6 +9,15 @@ const app = express()
 const port = 5000
 const isLogin = true
 let blogs = [] // global variable
+
+app.use(flash())
+
+app.use(session({
+    secret: 'rahasia',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 2 }
+}))
 
 // Testing connection database
 db.connect((err, _, done) => {
@@ -34,10 +46,12 @@ app.post('/form-blog', (req, res) => {
     // Destructuring assignment
     const { title, content, image } = req.body
 
+    const user_id = req.session.user.id
+
     db.connect((err, client, done) => {
         if (err) throw err
 
-        const query = `INSERT INTO tb_blog(title, content, image) VALUES ('${title}', '${content}', '${image}');`
+        const query = `INSERT INTO tb_blog(title, content, image, author_id) VALUES ('${title}', '${content}', '${image}', '${user_id}');`
 
         client.query(query, (err) => {
             if (err) throw err
@@ -46,7 +60,6 @@ app.post('/form-blog', (req, res) => {
             res.redirect('/blog')
         })
     })
-
 })
 
 app.get('/form-blog', (req, res) => {
@@ -54,6 +67,8 @@ app.get('/form-blog', (req, res) => {
 })
 
 app.get('/blog', (req, res) => {
+
+    // console.log(req.session);
 
     db.connect((err, client, done) => {
         if (err) {
@@ -72,13 +87,13 @@ app.get('/blog', (req, res) => {
                     ...blog,
                     author: `Mr. Jody Septiawan`,
                     time: getFullTime(blog.post_at),
-                    isLogin: isLogin
+                    isLogin: req.session.isLogin
                 }
         
                 return newBlog
             })
 
-            res.render('blog', { isLogin: isLogin, blogs: newBlogs })
+            res.render('blog', { user: req.session.user, isLogin: req.session.isLogin, blogs: newBlogs })
         })
         done()
     })
@@ -119,6 +134,84 @@ app.get('/delete-blog/:id', (req,res) => {
             res.redirect('/blog')
         })
     })
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+
+    const hashPassword = bcrypt.hashSync(password, 10)
+
+
+    db.connect((err, client, done) => {
+        if (err) throw err
+
+        const query = `INSERT INTO tb_user(name, email, password) VALUES ('${name}', '${email}', '${hashPassword}');`
+        client.query(query, (err) => {
+            if (err) throw err
+        })
+        
+        done()
+        req.flash('success', `Email: <b>${email}</b> has been register ✅`)
+        res.redirect('/login')
+    })
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body
+
+    if (email == '' || password == '') {
+        req.flash('warning', 'Please insert all fields')
+        return res.redirect('/login')
+    }
+
+    db.connect((err, client, done) => {
+        if (err) throw err
+
+        const query = `SELECT * FROM tb_user WHERE email = '${email}'`
+
+        client.query(query, (err, result) => {
+            if (err) throw err
+
+            const data = result.rows
+
+            // Check email
+            if (data.length == 0) {
+                req.flash('error', `Email: <b>${email}</b> not found ❌`)
+                return res.redirect('/login')
+            }
+
+            // Check Password
+            const isMatch = bcrypt.compareSync(password, data[0].password)
+
+            if (isMatch == false) {
+                req.flash('error', `Password Wrong! ❌`)
+                return res.redirect('/login')
+            }
+
+            // Store data to session
+            req.session.isLogin = true
+            req.session.user = {
+                id: data[0].id,
+                email: data[0].email,
+                name: data[0].name
+            }
+
+            res.redirect('/blog')
+        })
+    })
+})
+
+app.get('/logout', (req, res) => {
+    req.session.destroy()
+    res.redirect('/blog')
 })
 
 // Not found route custome
